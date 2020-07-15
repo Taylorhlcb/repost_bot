08267da2fb4142c32fcebdd7b42ab011e199de4e
Taylor Halcomb <taylortextalert@gmail.com>
2020-07-14 23:38:23 -0700

    re-added same user ignoring for response message

--- repost_bot/repost_bot.py
+++ repost_bot/repost_bot.py
@@ -382,7 +382,7 @@ class repost_bot:
                         file_path,\
                         file_id,\
                         message_id)
-                    if response['average_hash']['exists']:
+                    if response['average_hash']['exists'] and message[key]['from']['id'] != response['average_hash']['user_id']:
                         date_calc = str(datetime.utcfromtimestamp(message[key]['date']) - datetime.utcfromtimestamp(response['average_hash']['date']))
                         if "days" not in date_calc:
                             date_calc = "0 days, " + date_calc
